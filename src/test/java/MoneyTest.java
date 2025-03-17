3aba3ff1b003fa4b7edfff91f106f99268a2c19d
soy5360 <soy5360@gmail.com>
2025-03-18 01:03:35 +0900

    test: $5 * 2 = $10

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -11,15 +11,20 @@ public class MoneyTest {
     }
 
     class Dollar {
-        Dollar(int amount) {
+        /*가장 쉽게 테스트를 통과하는 방법*/
+//          int amount = 10;
 
+//        int amount = 5 * 2;
+
+        int amount;
+        Dollar(int amount) {
+            this.amount = amount;
         }
 
         void times(int multiplier) {
-
+            amount *= multiplier;
         }
 
-        int amount;
     }
 
 }
