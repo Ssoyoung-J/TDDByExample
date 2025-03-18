605436bf39ee64a7d351d6c201b0e1224b8dd6c0
JANGSOYOUNG\ZETALUX <soy5360@zetalux.co.kr>
2025-03-18 12:37:14 +0900

    test: 5CHF * 2 = 10CHF

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -11,6 +11,13 @@ public class MoneyTest {
     }
 
     @Test
+    public void testFrancMultiplication() {
+        Franc five = new Franc(5);
+        assertEquals(new Franc(10), five.times(2));
+        assertEquals(new Franc(15), five.times(3));
+    }
+
+    @Test
     public void testEquality() {
         assertTrue(new Dollar(5).equals(new Dollar(5)));
         assertFalse(new Dollar(5).equals(new Dollar(6)));
@@ -27,8 +34,8 @@ public class MoneyTest {
             this.amount = amount;
         }
         /*
-        * 올바른 금액을 갖는 new Dollar return
-        * */
+         * 올바른 금액을 갖는 new Dollar return
+         * */
         Dollar times(int multiplier) {
             return new Dollar(amount * multiplier);
         }
@@ -37,6 +44,31 @@ public class MoneyTest {
 //            return true;
             Dollar dollar = (Dollar) object;
             return amount == dollar.amount;
+        }
+
+    }
+
+    class Franc {
+        /*가장 쉽게 테스트를 통과하는 방법*/
+//          int amount = 10;
+
+//        int amount = 5 * 2;
+
+        private int amount;
+        Franc(int amount) {
+            this.amount = amount;
+        }
+        /*
+        * 올바른 금액을 갖는 new Dollar return
+        * */
+        Franc times(int multiplier) {
+            return new Franc(amount * multiplier);
+        }
+
+        public boolean equals(Object object) {
+//            return true;
+            Franc franc = (Franc) object;
+            return amount == franc.amount;
         }
 
     }
