fbc3fe86a6b2f4dc6c23d4f0b86419803d3f6f69
JANGSOYOUNG\ZETALUX <soy5360@zetalux.co.kr>
2025-03-18 12:27:29 +0900

    test: amount를 private으로 만들기

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -6,10 +6,8 @@ public class MoneyTest {
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
-        Dollar product = five.times(2);
-        assertEquals(10, product.amount);
-        product = five.times(3);
-        assertEquals(15, product.amount);
+        assertEquals(new Dollar(10), five.times(2));
+        assertEquals(new Dollar(15), five.times(3));
     }
 
     @Test
@@ -24,7 +22,7 @@ public class MoneyTest {
 
 //        int amount = 5 * 2;
 
-        int amount;
+        private int amount;
         Dollar(int amount) {
             this.amount = amount;
         }
