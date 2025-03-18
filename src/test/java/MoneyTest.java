72b43ef7d6e4cd5b24df4bb319048165210f6f5a
soy5360 <soy5360@gmail.com>
2025-03-18 21:56:34 +0900

    test: Dollar/Franc 중복

--- src/test/java/MoneyTest.java
+++ src/test/java/MoneyTest.java
@@ -17,14 +17,12 @@ public class MoneyTest {
 //        assertEquals(new Franc(15), five.times(3));
 //    }
 //
-//    @Test
-//    public void testEquality() {
-//        assertTrue(new Dollar(5).equals(new Dollar(5)));
-//        assertFalse(new Dollar(5).equals(new Dollar(6)));
-//        assertTrue(new Franc(5).equals(new Franc(5)));
-//        assertFalse(new Franc(5).equals(new Franc(6)));
-//        assertFalse(new Franc(5).equals(new Dollar(5)));
-//    }
+    @Test
+    public void testEquality() {
+        assertTrue(Money.dollar(5).equals(Money.dollar(5)));
+        assertFalse(Money.dollar(5).equals(Money.dollar(6)));
+        assertTrue(Money.franc(5).equals(Money.franc(5)));
+    }
 
     @Test
     public void testCurrency() {
